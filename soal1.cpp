a1c5abb0a3ed40c162351d7807a999783ed68cb4
agusdarma-lab <agusdarma281@gmail.com>
2025-09-27 10:06:46 +0800

    first commit

---
+++ soal1.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+float j, t, v;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    cin >> j >> t;
+    v = j / t;
+    printf("%.3f", v);
+    return 0;
+}
